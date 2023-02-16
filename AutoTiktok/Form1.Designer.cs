519b24770095e13c4a5af01e5818d4f0c7633781
tahacong@gmail.com <tahacong@gmail.com>
2023-02-16 16:16:14 +0700

    Signed-off-by: tahacong@gmail.com <tahacong@gmail.com>

--- AutoTiktok/Form1.Designer.cs
+++ AutoTiktok/Form1.Designer.cs
@@ -28,12 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtConfig = new System.Windows.Forms.TextBox();
+            this.txtLinkAcc = new System.Windows.Forms.TextBox();
+            this.btn_acc = new System.Windows.Forms.Button();
+            this.btn_ConfigB = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtConfig
+            // 
+            this.txtConfig.Location = new System.Drawing.Point(97, 697);
+            this.txtConfig.Name = "txtConfig";
+            this.txtConfig.Size = new System.Drawing.Size(563, 20);
+            this.txtConfig.TabIndex = 0;
+            // 
+            // txtLinkAcc
+            // 
+            this.txtLinkAcc.Location = new System.Drawing.Point(97, 660);
+            this.txtLinkAcc.Name = "txtLinkAcc";
+            this.txtLinkAcc.Size = new System.Drawing.Size(563, 20);
+            this.txtLinkAcc.TabIndex = 1;
+            // 
+            // btn_acc
+            // 
+            this.btn_acc.Location = new System.Drawing.Point(666, 658);
+            this.btn_acc.Name = "btn_acc";
+            this.btn_acc.Size = new System.Drawing.Size(75, 23);
+            this.btn_acc.TabIndex = 2;
+            this.btn_acc.Text = "Brower";
+            this.btn_acc.UseVisualStyleBackColor = true;
+            this.btn_acc.Click += new System.EventHandler(this.btn_acc_Click);
+            // 
+            // btn_ConfigB
+            // 
+            this.btn_ConfigB.Location = new System.Drawing.Point(666, 695);
+            this.btn_ConfigB.Name = "btn_ConfigB";
+            this.btn_ConfigB.Size = new System.Drawing.Size(75, 23);
+            this.btn_ConfigB.TabIndex = 3;
+            this.btn_ConfigB.Text = "Brower";
+            this.btn_ConfigB.UseVisualStyleBackColor = true;
+            this.btn_ConfigB.Click += new System.EventHandler(this.btn_ConfigB_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.btn_ConfigB);
+            this.Controls.Add(this.btn_acc);
+            this.Controls.Add(this.txtLinkAcc);
+            this.Controls.Add(this.txtConfig);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtConfig;
+        private System.Windows.Forms.TextBox txtLinkAcc;
+        private System.Windows.Forms.Button btn_acc;
+        private System.Windows.Forms.Button btn_ConfigB;
     }
 }
 
--- AutoTiktok/Form1.cs
+++ AutoTiktok/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using RestSharp;
+using RestSharp.Authenticators;
+using System.Net;
 
 namespace AutoTiktok
 {
@@ -15,6 +18,60 @@ namespace AutoTiktok
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //Thay đổi IP cho DCOM
+        public static string ResetDCOM(string modemUrl, string username, string password)
+        {
+            var client = new RestClient(modemUrl);
+            client.Authenticator = new HttpBasicAuthenticator(username, password);
+
+            var request = new RestRequest("api/dialup/mobiledata", Method.POST);
+            request.AddParameter("xml", "<?xml version=\"1.0\" encoding=\"UTF-8\"?><request><Control>1</Control></request>");
+
+            IRestResponse response = client.Execute(request);
+
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                // handle error
+                return "Error: " + response.ErrorMessage;
+            }
+            else
+            {
+                return "IP address changed successfully";
+            }
+        }
+        //Chọn đường dẫn File chứa tài khoản mật khẩu tiktok
+        private void btn_acc_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Title = "Chọn tệp tin";
+            openFileDialog1.Filter = "Tệp tin văn bản (*.txt)|*.txt|Tất cả các tệp tin (*.*)|*.*";
+            openFileDialog1.InitialDirectory = "C:\\";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtLinkAcc.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void btn_ConfigB_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Title = "Chọn tệp tin";
+            openFileDialog1.Filter = "Tệp tin văn bản (*.txt)|*.txt|Tất cả các tệp tin (*.*)|*.*";
+            openFileDialog1.InitialDirectory = "C:\\";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtConfig.Text = openFileDialog1.FileName;
+            }
         }
     }
 }
