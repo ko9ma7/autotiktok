5281555d3f5dbe9ef8b710e4017d5fae76eaedb4
unknown <tahacong@gmail.com>
2023-02-20 13:33:10 +0700

    Signed-off-by: unknown <tahacong@gmail.com>

--- AutoTiktok/Form1.cs
+++ AutoTiktok/Form1.cs
@@ -11,7 +11,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using KAutoHelper;
+using System.Drawing;
 
 
 namespace AutoTiktok
@@ -23,6 +23,7 @@ namespace AutoTiktok
         {
             InitializeComponent();
         }
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -126,68 +127,9 @@ namespace AutoTiktok
                 driver.Navigate().GoToUrl(url);
             }
         }
-        //Đăng ký tai khoan băng email
+        //Đăng nhâp tiktok
 
-        private void RegisterTikTokAccount()
-        {
-            // Launch TikTok app
-            //ADBHelper.ExecuteAdbCommand
-            ADBHelper.ExecuteCMD("shell am start -n com.ss.android.ugc.trill/com.ss.android.ugc.aweme.splash.SplashActivity");
-
-            // Wait for TikTok app to launch
-            Thread.Sleep(5000);
-
-            // Tap on register button
-            ADBHelper.Tap("SABO_LD_PLAYER", 1050, 620);
-
-            // Wait for register page to load
-            Thread.Sleep(5000);
-
-            // Tap on phone number button
-            ADBHelper.Tap("SABO_LD_PLAYER", 530, 960);
-
-            // Wait for phone number page to load
-            Thread.Sleep(5000);
-
-            // Tap on input box for phone number
-            ADBHelper.Tap("SABO_LD_PLAYER", 470, 470);
-
-            // Input phone number
-            KAutoHelper.ADBHelper.InputText("Emulator-5554","0987654321");
-
-            // Tap on submit button
-            ADBHelper.Tap("SABO_LD_PLAYER", 530, 680);
-
-            // Wait for verification page to load
-            Thread.Sleep(5000);
-
-            // Tap on input box for verification code
-            ADBHelper.Tap("SABO_LD_PLAYER", 480, 515);
-
-            // Input verification code
-            KAutoHelper.ADBHelper.InputText("Emulator-5554", "0987654321");
-
-            // Tap on submit button
-            ADBHelper.Tap("SABO_LD_PLAYER", 530, 680);
-
-            // Wait for profile setup page to load
-            Thread.Sleep(5000);
-
-            // Tap on username input box
-            ADBHelper.Tap("SABO_LD_PLAYER", 510, 425);
-
-            // Input username
-            KAutoHelper.ADBHelper.InputText("Emulator-5554", "0987654321");
-
-            // Tap on password input box
-            ADBHelper.Tap("SABO_LD_PLAYER", 510, 540);
-
-            // Input password
-            KAutoHelper.ADBHelper.InputText("Emulator-5554", "0987654321");
-
-            // Tap on submit button
-            ADBHelper.Tap("SABO_LD_PLAYER", 530, 680);
-        }
+       
         private void btn_Start_Click(object sender, EventArgs e)
         {
             OpenLD();
@@ -200,8 +142,13 @@ namespace AutoTiktok
 
         private void btn_sortLD_Click(object sender, EventArgs e)
         {
-            // sortLDWindows();
-            RegisterTikTokAccount();
+            string deviceID = null;
+            var listDevice = KAutoHelper.ADBHelper.GetDevices();
+            if (listDevice != null && listDevice.Count > 0)
+            {
+                deviceID = listDevice[0];
+                MessageBox.Show(deviceID);
+            }
         }
 
         private void btn_Close_Click(object sender, EventArgs e)
